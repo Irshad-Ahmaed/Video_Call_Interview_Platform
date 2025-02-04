ce2edb5ac25076f59b4890b398a3f7e85bae7aae
IRSHAD AHMAD <129326052+Irshad-Ahmaed@users.noreply.github.com>
2025-02-04 22:32:26 +0530

    http.ts + users.ts with webhook api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
+import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <SignedOut>
-        <SignInButton />
+        <Button variant='default' size='default'><SignInButton /></Button>
       </SignedOut>
       <SignedIn>
-        <UserButton />
+        <Button variant='secondary' size='icon'><UserButton /></Button>
       </SignedIn>
     </div>
   );
